bb3e5ce3a7438e0cec64a95d83612b13762b199b
SergioGaV <sergiogav@comunidad.unam.mx>
2021-10-07 13:43:30 -0500

    Actualiza brazos, ejercicio, index y pectoral
    Agrega la funcionalidad para navegar a las páginas.

--- control1.js
+++ control1.js
@@ -2,10 +2,7 @@ var x = 0;
 var y = 0;
 var k = 0;
 let current = 0; 
-var btn = ["brazo", "cardio", "pierna", "abdomen", "espalda", "elastico"];
- focusableElements = document.querySelectorAll(
-'button'
-);
+focusableElements = document.querySelectorAll('button, area, iframe');
 console.log(focusableElements);
 const rAF = window.mozRequestAnimationFrame || window.requestAnimationFrame; 
 window.addEventListener('gamepadconnected', event => {
@@ -53,12 +50,12 @@ function update () {
           {button_13: gamepads [0] .buttons [13] .pressed},
         ]
       }
-      const flechas = gamepads [0] .axes [9] .toFixed (3); // Flechas para izquierda o derecha
-      console.log(flechas);
+    //   const flechas = gamepads [0] .axes [9] .toFixed (3); // Flechas para izquierda o derecha
+    //   console.log(flechas);
       if (gamepads [0] .axes [9] .toFixed(3) == -0.429) { //Derecha
         x = x + 4;
         k = k + 1 ;
-        current = k % btn.length;
+        current = k % focusableElements.length;
         console.log(current);
         console.log ('movido', x);
         // document.getElementById(btn[current]).focus(); 
@@ -69,41 +66,41 @@ function update () {
         x = x - 4;
         k = k - 1 ;
         if(k==-1){
-          k = 5;
+          k = 1;
         }
-        current = k % btn.length;
+        current = k % focusableElements.length;
         console.log(current);
         console.log ('movido', x);
         // document.getElementById(btn[current]).focus();
         focusableElements[current].focus();   
       }
-      if (gamepads [0] .axes [9] .toFixed(3) == -1.000) { //Arriba
-        y = y + 4;
-        k = k - 3 ;
-        if(k==-2){
-          k = 4;
-        }
-        if(k==-3){
-          k = 3;
-        }
-        if(k==-1){
-          k = 5;
-        }
-        current = k % btn.length;
-        console.log(current);
-        console.log ('movido', y);
-        // document.getElementById(btn[current]).focus(); 
-        focusableElements[current].focus(); 
-      }
-      if (gamepads [0] .axes [9] .toFixed(3) == 0.143) { //Abajo
-        y = y - 4;
-        console.log ('movido', y);
-        console.log(current);
-        k = k + 3 ;
-        current = k % btn.length;
-        // document.getElementById(btn[current]).focus();  
-        focusableElements[current].focus(); 
-      }
+//       if (gamepads [0] .axes [9] .toFixed(3) == -1.000) { //Arriba
+//         y = y + 4;
+//         k = k - 3 ;
+//         if(k==-2){
+//           k = 4;
+//         }
+//         if(k==-3){
+//           k = 3;
+//         }
+//         if(k==-1){
+//           k = 5;
+//         }
+//         current = k % btn.length;
+//         console.log(current);
+//         console.log ('movido', y);
+//         // document.getElementById(btn[current]).focus(); 
+//         focusableElements[current].focus(); 
+//       }
+//       if (gamepads [0] .axes [9] .toFixed(3) == 0.143) { //Abajo
+//         y = y - 4;
+//         console.log ('movido', y);
+//         console.log(current);
+//         k = k + 3 ;
+//         current = k % btn.length;
+//         // document.getElementById(btn[current]).focus();  
+//         focusableElements[current].focus(); 
+//       }
    if(gamepads [0] .buttons [2] .pressed){
     // document.getElementById(btn[current]).click();
     focusableElements[current].click(); 
@@ -111,4 +108,3 @@ function update () {
   }
   setTimeout(() => rAF(update), 100)
 }
-

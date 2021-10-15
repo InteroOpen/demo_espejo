125faff7884c2a49dbadbd1fdcf7f2109deac88a
SergioGaV <sergiogav@comunidad.unam.mx>
2021-10-15 13:01:17 -0500

    Actualiza control, control1 y ejercicio
    Añade el mapeo del tapete

--- control.js
+++ control.js
@@ -53,19 +53,71 @@ function update () {
       //     {button_13: gamepads [0] .buttons [13] .pressed},
       //   ]
       // }
-      const flechas = gamepads [0] .axes [9] .toFixed (3); // Flechas para izquierda o derecha
-      console.log(flechas);
-      if (gamepads [0] .axes [9] .toFixed(3) == -0.429) { //Derecha
-        x = x + 4;
-        k = k + 1 ;
-        current = k % btn.length;
-        console.log(current);
-        console.log ('movido', x);
-        // document.getElementById(btn[current]).focus(); 
-        focusableElements[current].focus(); 
-      }
+      // const flechas = gamepads [0] .axes [9] .toFixed (3); // Flechas para izquierda o derecha
+      // console.log(flechas);
+      // if (gamepads [0] .axes [9] .toFixed(3) == -0.429) { //Derecha
+      //   x = x + 4;
+      //   k = k + 1 ;
+      //   current = k % btn.length;
+      //   console.log(current);
+      //   console.log ('movido', x);
+      //   // document.getElementById(btn[current]).focus(); 
+      //   focusableElements[current].focus(); 
+      // }
    
-      if (gamepads [0] .axes [9] .toFixed(3) == 0.714) { //Izquierda
+      // if (gamepads [0] .axes [9] .toFixed(3) == 0.714) { //Izquierda
+      //   x = x - 4;
+      //   k = k - 1 ;
+      //   if(k==-1){
+      //     k = 5;
+      //   }
+      //   current = k % btn.length;
+      //   console.log(current);
+      //   console.log ('movido', x);
+      //   // document.getElementById(btn[current]).focus();
+      //   focusableElements[current].focus();   
+      // }
+      // if (gamepads [0] .axes [9] .toFixed(3) == -1.000) { //Arriba
+      //   y = y + 4;
+      //   k = k - 3 ;
+      //   if(k==-2){
+      //     k = 4;
+      //   }
+      //   if(k==-3){
+      //     k = 3;
+      //   }
+      //   if(k==-1){
+      //     k = 5;
+      //   }
+      //   current = k % btn.length;
+      //   console.log(current);
+      //   console.log ('movido', y);
+      //   // document.getElementById(btn[current]).focus(); 
+      //   focusableElements[current].focus(); 
+      // }
+      // if (gamepads [0] .axes [9] .toFixed(3) == 0.143) { //Abajo
+      //   y = y - 4;
+      //   console.log ('movido', y);
+      //   console.log(current);
+      //   k = k + 3 ;
+      //   current = k % btn.length;
+      //   // document.getElementById(btn[current]).focus();  
+      //   focusableElements[current].focus(); 
+      // }
+  //  if(gamepads [0] .buttons [2] .pressed){
+  //   // document.getElementById(btn[current]).click();
+  //   focusableElements[current].click(); 
+  //  }
+  if (gamepads [0] .buttons [2] .pressed) { //Derecha
+    x = x + 4;
+    k = k + 1 ;
+    current = k % btn.length;
+    console.log(current);
+    console.log ('movido', x);
+    // document.getElementById(btn[current]).focus(); 
+    focusableElements[current].focus(); 
+  }
+  if (gamepads [0] .buttons [3] .pressed) { //Izquierda
         x = x - 4;
         k = k - 1 ;
         if(k==-1){
@@ -77,7 +129,7 @@ function update () {
         // document.getElementById(btn[current]).focus();
         focusableElements[current].focus();   
       }
-      if (gamepads [0] .axes [9] .toFixed(3) == -1.000) { //Arriba
+  if (gamepads [0] .buttons [0] .pressed) { //Arriba
         y = y + 4;
         k = k - 3 ;
         if(k==-2){
@@ -95,7 +147,7 @@ function update () {
         // document.getElementById(btn[current]).focus(); 
         focusableElements[current].focus(); 
       }
-      if (gamepads [0] .axes [9] .toFixed(3) == 0.143) { //Abajo
+      if (gamepads [0] .buttons [1] .pressed) { //Abajo
         y = y - 4;
         console.log ('movido', y);
         console.log(current);
@@ -104,15 +156,20 @@ function update () {
         // document.getElementById(btn[current]).focus();  
         focusableElements[current].focus(); 
       }
-   if(gamepads [0] .buttons [2] .pressed){
-    // document.getElementById(btn[current]).click();
-    focusableElements[current].click(); 
-   }
+      if(gamepads [0] .buttons [9] .pressed){
+        // document.getElementById(btn[current]).click();
+        focusableElements[current].click(); 
+      //  }
   }
   setTimeout(() => rAF(update), 100)
 }
+}
 
-      // gamepads [0] .buttons [2] .pressed//Derecha
-      // gamepads [0] .buttons [3] .pressed//Izquierda
-      // gamepads [0] .buttons [0] .pressed//Arriba
-      // gamepads [0] .buttons [1] .pressed//Abajo
+      //Mapeo Tapete
+      // gamepads [0] .buttons [2] .pressed //Derecha
+      // gamepads [0] .buttons [3] .pressed //Izquierda
+      // gamepads [0] .buttons [0] .pressed //Arriba
+      // gamepads [0] .buttons [1] .pressed //Abajo
+      // gamepads [0] .buttons [8] .pressed //Back
+      // gamepads [0] .buttons [9] .pressed //Start
+      

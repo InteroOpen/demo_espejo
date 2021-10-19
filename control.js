f10e44045efdb4da8112edc12b19e3ec1323a410
SergioGaV <sergiogav@comunidad.unam.mx>
2021-10-19 14:13:23 -0500

    Actualiza script

--- control.js
+++ control.js
@@ -19,37 +19,6 @@ window.addEventListener('gamepaddisconnected', event => {
 function update () {
     const gamepads = navigator.getGamepads ();
     if (gamepads [0]) {
-      const gamepadState = {
-        id: gamepads [0] .id,
-        ejes: [
-          gamepads [0] .axes [0] .toFixed (2),
-          gamepads [0] .axes [1] .toFixed (2),
-          gamepads [0] .axes [2] .toFixed (2),
-          gamepads [0] .axes [3] .toFixed (2),
-          gamepads [0] .axes [4] .toFixed (2),
-          gamepads [0] .axes [5] .toFixed (2),
-          gamepads [0] .axes [6] .toFixed (2),
-          gamepads [0] .axes [7] .toFixed (2),
-          gamepads [0] .axes [8] .toFixed (2),
-          gamepads [0] .axes [9] .toFixed (2),
-        ],
-        botones: [
-          {button_0: gamepads [0] .buttons [0] .pressed},
-          {button_1: gamepads [0] .buttons [1] .pressed},
-          {button_2: gamepads [0] .buttons [2] .pressed},
-          {button_3: gamepads [0] .buttons [3] .pressed},
-          {button_4: gamepads [0] .buttons [4] .pressed},
-          {button_5: gamepads [0] .buttons [5] .pressed},
-          {button_6: gamepads [0] .buttons [6] .pressed},
-          {button_7: gamepads [0] .buttons [7] .pressed},
-          {button_8: gamepads [0] .buttons [8] .pressed},
-          {button_9: gamepads [0] .buttons [9] .pressed},
-          {button_10: gamepads [0] .buttons [10] .pressed},
-          {button_11: gamepads [0] .buttons [11] .pressed},
-          {button_12: gamepads [0] .buttons [12] .pressed},
-          {button_13: gamepads [0] .buttons [13] .pressed},
-        ]
-      }
     //   const flechas = gamepads [0] .axes [9] .toFixed (3); // Flechas para izquierda o derecha
     //   console.log(flechas);
       if (gamepads [0] .axes [9] .toFixed(3) == -0.429) { //Derecha
@@ -66,7 +35,7 @@ function update () {
         x = x - 4;
         k = k - 1 ;
         if(k==-1){
-          k = 1;
+          k = 5;
         }
         current = k % focusableElements.length;
         console.log(current);
@@ -97,7 +66,7 @@ function update () {
         console.log ('movido', y);
         console.log(current);
         k = k + 3 ;
-        current = k %focusableElements.length;
+        current = k % focusableElements.length;
         // document.getElementById(btn[current]).focus();  
         focusableElements[current].focus(); 
       }
@@ -106,5 +75,6 @@ function update () {
     focusableElements[current].click(); 
    }
   }
-  setTimeout(() => rAF(update), 100)
-}
+   setTimeout(() => rAF(update), 100)
+  }
+

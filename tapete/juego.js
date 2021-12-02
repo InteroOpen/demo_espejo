d3d99189691dc754c068300f4279495c7169230f
savashito <savashito@rocks-MacBook-Air.local>
2021-12-02 17:40:39 -0500

    Agregamos 3 distintos colores

---
+++ tapete/juego.js
@@ -0,0 +1,14 @@
+acciones = [
+	{
+		duracion: 5,
+		id_flechas: None,
+	}, 
+	{
+		duracion: 5,
+		id_flechas: ["flecha_centro_derecha", "flecha_centro_izquierda"]
+	},
+	{
+		duracion: 5,
+		id_flechas: ["flecha_central"]
+	},
+]
